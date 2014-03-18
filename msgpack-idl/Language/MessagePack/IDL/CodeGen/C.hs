9326aa2211973cb111be88de57c85df07481af16
takayuki <takayuki.sato.dev@gmail.com>
2014-03-18 20:33:19 +0900

    fixed TMap

--- msgpack-idl/Language/MessagePack/IDL/CodeGen/C.hs
+++ msgpack-idl/Language/MessagePack/IDL/CodeGen/C.hs
@@ -5,8 +5,6 @@ module Language.MessagePack.IDL.CodeGen.C (
   generate,
   ) where
 
-import Data.List
-import Data.Monoid
 import Text.Printf
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
@@ -90,43 +88,29 @@ int #{configPrefix c}_#{msgName}_from_msgpack(msgpack_object *obj, #{msgName} *a
 genTypeImpl _ _ = ""
 
 
-
-sortField :: [Field] -> [Maybe Field]
-sortField flds =
-  flip map [0 .. maximum $ [-1] ++ map fldId flds] $ \ix ->
-  find ((==ix). fldId) flds
-
-genClient _ = ""
-
-sanitize :: Char -> Char
-sanitize '[' = '_'
-sanitize ']' = '_'
-sanitize c = c
-
 toMsgpack :: Config -> Type -> T.Text -> LT.Text
 toMsgpack _ (TInt _ _) arg = [lt|msgpack_pack_int(pk, #{arg});|]
 toMsgpack _ (TFloat _) arg = [lt|msgpack_pack_double(pk, #{arg});|]
-toMsgpack _ TBool arg = [lt|(arg)?msgpack_pack_true(pk):msgpack_pack_false(pk)});|]
+toMsgpack _ TBool arg = [lt|#{arg} ? msgpack_pack_true(pk) : msgpack_pack_false(pk)});|]
 toMsgpack _ TString arg = [lt|do {
     size_t _l = strlen(#{arg});
     msgpack_pack_raw(pk, _l);
     msgpack_pack_raw_body(pk, #{arg}, _l);
 } while(0);|]
 
-toMsgpack c (TList t) arg = [lt|msgpack_pack_array(pk, #{lis_len});
-for (int _i = 0; i < #{lis_len}; ++_i) {
+toMsgpack c (TList t) arg = [lt|msgpack_pack_array(pk, #{lis_size});
+for (int _i = 0; i < #{lis_size}; ++_i) {
 #{indent4 $ toMsgpack c t ith_val}}|]
   where
-    lis_len = T.append arg (T.pack "_len")
+    lis_size = T.append arg (T.pack "_size")
     ith_val = T.append arg (T.pack "[_i]")
 
 
-toMsgpack c (TMap t1 t2) arg = [lt|msgpack_pack_map(pk, #{map_len});
-for (int _i = 0; i < #{map_len}; ++_i) {
-#{indent4 $ toMsgpack c t1 ith_key}
-#{indent4 $ toMsgpack c t2 ith_val}}|]
+toMsgpack c (TMap t1 t2) arg = [lt|msgpack_pack_map(pk, #{map_size});
+for (int _i = 0; i < #{map_size}; ++_i) {
+#{indent4 $ toMsgpack c t1 ith_key}#{indent4 $ toMsgpack c t2 ith_val}}|]
   where
-    map_len = T.append arg (T.pack "_len")
+    map_size = T.append arg (T.pack "_size")
     ith_key = T.append (T.cons '&' arg) (T.pack "_keys[_i]")
     ith_val = T.append (T.cons '&' arg) (T.pack "_vals[_i]")
 
@@ -138,10 +122,10 @@ toMsgpack c (TNullable t) arg = [lt|if (!#{arg}) {
 toMsgpack c (TPointer t) arg = [lt|if (!#{arg}) {
     msgpack_pack_null(pk);
 } else {
-#{indent4 $ toMsgpack' t arg}}|]
+#{indent4 $ toMsgpack' t}}|]
   where
-    toMsgpack' (TUserDef name _) arg = [lt|#{configPrefix c}_#{name}_to_msgpack(pk, #{arg});|]
-    toMsgpack' t arg = toMsgpack c t (T.cons '*' arg)
+    toMsgpack' (TUserDef name _) = [lt|#{configPrefix c}_#{name}_to_msgpack(pk, #{arg});|]
+    toMsgpack' t' = toMsgpack c t' (T.cons '*' arg)
 
 toMsgpack c (TUserDef name _) arg = [lt|#{configPrefix c}_#{name}_to_msgpack(pk, &#{arg});|]
 toMsgpack _ t _ = [lt|/* #{show t} unsupported */|]
@@ -155,15 +139,23 @@ fromMsgpack _ o (TInt _ _) _ arg = [lt|#{arg} = #{o}.via.i64;|]
 fromMsgpack _ o (TFloat _) _ arg = [lt|#{arg} = #{o}.via.dec;|]
 fromMsgpack c o (TNullable t) d arg = fromMsgpack c o t d arg
 fromMsgpack _ o TBool _ arg = [lt|#{arg} = #{o}.via.boolean;|]
-fromMsgpack _ o TString _ arg = [lt|#{arg} = #{o}.via.raw.ptr;|]
+fromMsgpack _ o TString _ arg = [lt|#{arg} = strdup(#{o}.via.raw.ptr);|]
 fromMsgpack c o (TUserDef name _) _ arg = [lt|#{configPrefix c}_#{name}_from_msgpack(&#{o}, &#{arg});|]
 
-fromMsgpack c o (TList t) d arg = [lt|#{lis_len} = #{o}.via.array.size;
+fromMsgpack c o (TList t) d arg = [lt|#{lis_size} = #{o}.via.array.size;
 for (int _i = 0; _i < #{o}.via.array.size; ++_i) {
-#{indent4 $ fromMsgpack c o t d ith_val}}|]
+#{indent4 $ fromMsgpack c (T.pack $ printf "%s.via.array.ptr[_i]" $ T.unpack o) t d ith_val}}|]
   where
-    lis_len = T.append arg (T.pack "_len")
+    lis_size = T.append arg (T.pack "_size")
     ith_val = T.append arg (T.pack "[_i]")
+
+fromMsgpack c o (TMap t1 t2) d arg = [lt|#{map_size} = #{o}.via.map.size;
+for (int _i = 0; _i < #{o}.via.map.size; ++_i) {
+#{indent4 $ fromMsgpack c (T.pack $ printf "%s.via.map.ptr[_i].key" $ T.unpack o) t1 d ith_key}#{indent4 $ fromMsgpack c (T.pack $ printf "%s.via.map.ptr[_i].val" $ T.unpack o) t2 d ith_val}}|]
+  where
+    map_size = T.append arg (T.pack "_size")
+    ith_key = T.append arg (T.pack "_keys[_i]")
+    ith_val = T.append arg (T.pack "_vals[_i]")
 
 
 fromMsgpack c o (TPointer t) d arg = [lt|if (#{o}.type == MSGPACK_OBJECT_NIL) {
@@ -172,14 +164,15 @@ fromMsgpack c o (TPointer t) d arg = [lt|if (#{o}.type == MSGPACK_OBJECT_NIL) {
 #{indent4 $ fromMsgpack' t}}|]
   where
     fromMsgpack' (TUserDef name _) = [lt|#{configPrefix c}_#{name}_from_msgpack(&#{o}, #{arg});|]
-    fromMsgpack' t = fromMsgpack c o t d (T.cons '*' arg)
-    valsize (TInt _ _) = [lt|sizeof(#{o}.via.i64)|]
-    valsize (TFloat _) = [lt|sizeof(#{o}.via.dec)|]
-    valsize TBool = [lt|sizeof(#{o}.via.boolean)|]
-    valsize TString = [lt|#{o}.via.raw.size|]
-    valsize TRaw = [lt|#{o}.via.raw.size|]
-    valsize (TUserDef name _) = [lt|sizeof(#{name})|]
-    valsize _ = LT.pack "0"
+    fromMsgpack' t' = fromMsgpack c o t' d (T.cons '*' arg)
+    --   for malloc
+    -- valsize (TInt _ _) = [lt|sizeof(#{o}.via.i64)|]
+    -- valsize (TFloat _) = [lt|sizeof(#{o}.via.dec)|]
+    -- valsize TBool = [lt|sizeof(#{o}.via.boolean)|]
+    -- valsize TString = [lt|#{o}.via.raw.size|]
+    -- valsize TRaw = [lt|#{o}.via.raw.size|]
+    -- valsize (TUserDef name _) = [lt|sizeof(#{name})|]
+    -- valsize _ = LT.pack "0"
 
 
 fromMsgpack _ _ t _ _ = [lt|/* #{show t} unsupported */|]
